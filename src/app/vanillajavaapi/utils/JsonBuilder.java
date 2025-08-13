58a57e58396a1d73b6932023812fdaa9b38d4fdf
Ricardo Kanashiro <89729397+ricardokanashiro@users.noreply.github.com>
2025-08-13 16:05:25 -0300

    Fix JSON builder loop to omit trailing comma

--- src/app/vanillajavaapi/utils/JsonBuilder.java
+++ src/app/vanillajavaapi/utils/JsonBuilder.java
@@ -6,16 +6,15 @@ public class JsonBuilder {
 
         StringBuilder json = new StringBuilder("{");
 
-        for(int i = 0; i <= (parametros.length / 2); i += 2) {
-
+        for (int i = 0; i < parametros.length; i += 2) {
             String chave = parametros[i];
             String valor = parametros[i + 1];
 
             json.append("\"").append(chave).append("\"")
                     .append(":")
-                        .append("\"").append(valor).append("\"");
+                    .append("\"").append(valor).append("\"");
 
-            if(!(i == parametros.length / 2)) {
+            if (i + 2 < parametros.length) {
                 json.append(",");
             }
         }
